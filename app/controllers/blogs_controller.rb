d90098148b6c4e2a5711596fcdfa26d0a11a39c5
ueharakaede <yapaari.kaede.kakoii@gmail.com>
2021-09-15 07:18:47 +0000

    [start]3-5

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,9 +5,25 @@ class BlogsController < ApplicationController
   def show
   end
 
+  def index
+    @blogs = Blog.all
+  end
+  
   def new
+    @blog = Blog.new
   end
 
+  def create
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
+  end
+  
   def edit
+  end
+  
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :category, :body)
   end
 end
